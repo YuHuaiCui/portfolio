cbd2eb842a7c1d3c2738aac03cbef27945415bc1
Daniel Cui <cyhonline@pm.me>
2025-04-27 07:06:59 -0600

    Update route to use runtime

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,17 +1,22 @@
 export const runtime = 'edge'
 
-export async function GET() {
-  const pdfUrl = new URL('/DanielCui_Resume.pdf', import.meta.url)
+export async function GET(request: Request) {
+  const { origin } = new URL(request.url)
+
+  const pdfUrl = `${origin}/DanielCui_Resume.pdf`
+
   const res = await fetch(pdfUrl)
   if (!res.ok) {
     return new Response('Not found', { status: 404 })
   }
+
   const arrayBuffer = await res.arrayBuffer()
 
   return new Response(arrayBuffer, {
     status: 200,
     headers: {
       'Content-Type': 'application/pdf',
+      'Content-Length': arrayBuffer.byteLength.toString(),
       'Content-Disposition': 'attachment; filename="DanielCui_Resume.pdf"',
     },
   })
